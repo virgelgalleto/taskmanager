e1257bbe2e8783c41e8f496d1bcbc8a85847240c
24virgel@gmail.com <24virgel@gmail.com>
2025-09-05 20:18:56 +0800

    a

--- task-manager-api/src/db.js
+++ task-manager-api/src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/task_manager";
+  const uri = process.env.MONGODB_URI || "mongodb+srv://24virgel:8hCH2GmSp08fJhm9@taskmanager.sqpitl1.mongodb.net/?retryWrites=true&w=majority&appName=taskmanager";
   mongoose.set("strictQuery", false);
   await mongoose.connect(uri);
   console.log("✅ MongoDB connected");
